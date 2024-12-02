8d1dd85a47d4122aed2f7cbc7ee2313d0e7208bf
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-12-02 10:23:28 +0530

    Update Lab 1.sql

--- Lab 1.sql
+++ Lab 1.sql
@@ -5,13 +5,13 @@ SELECT DISTINCT GENRE FROM SONGS;
 --2. Find top 2 albums released before 2010.
 SELECT TOP 2 ALBUM_TITLE FROM ALBUMS WHERE RELEASE_YEAR<2010;
 
---3. Insert Data into the Songs Table. (1245, ‘Zaroor’, 2.55, ‘Feel good’, 1005)
+--3. Insert Data into the Songs Table. (1245, â€˜Zaroorâ€™, 2.55, â€˜Feel goodâ€™, 1005)
 INSERT INTO SONGS VALUES(1245,'ZAROOR',2.55,'FEEL GOOD',1005);
 
---4. Change the Genre of the song ‘Zaroor’ to ‘Happy’
+--4. Change the Genre of the song â€˜Zaroorâ€™ to â€˜Happyâ€™
 UPDATE SONGS SET GENRE='HAPPY' WHERE SONG_TITLE='ZAROOR';
 
---5. Delete an Artist ‘Ed Sheeran’
+--5. Delete an Artist â€˜Ed Sheeranâ€™
 DELETE FROM ARTISTS WHERE ARTIST_NAME='Ed Sheeran';
 
 --6. Add a New Column for Rating in Songs Table. [Ratings decimal(3,2)]
@@ -26,7 +26,7 @@ SELECT * FROM SONGS WHERE SONG_TITLE LIKE '%EVERYBODY%';
 --9. Display Artist Name in Uppercase.
 SELECT UPPER(ARTIST_NAME) AS ARTIST_NAME FROM ARTISTS;
 
---10. Find the Square Root of the Duration of a Song ‘Good Luck’.
+--10. Find the Square Root of the Duration of a Song â€˜Good Luckâ€™.
 SELECT SQRT(DURATION) AS DURATION_SQRT FROM SONGS WHERE SONG_TITLE='GOOD LUCK';
 
 --11. Find Current Date.
@@ -41,7 +41,7 @@ SELECT ALBUM_ID,COUNT(SONG_ID) AS NOOFSONGS FROM SONGS GROUP BY ALBUM_ID HAVING 
 --14. Retrieve all songs from the album 'Album1'. (using Subquery)
 SELECT SONG_TITLE FROM SONGS WHERE ALBUM_ID IN(SELECT ALBUM_ID FROM ALBUMS WHERE ALBUM_TITLE='ALBUM1');
 
---15. Retrieve all albums name from the artist ‘Aparshakti Khurana’ (using Subquery)
+--15. Retrieve all albums name from the artist â€˜Aparshakti Khuranaâ€™ (using Subquery)
 SELECT ALBUM_TITLE FROM ALBUMS WHERE ARTIST_ID IN(SELECT ARTIST_ID FROM ARTISTS WHERE ARTIST_NAME='Aparshakti Khurana');
 
 --16. Retrieve all the song titles with its album title.
@@ -55,13 +55,13 @@ FROM ALBUMS JOIN SONGS
 ON ALBUMS.ALBUM_ID=SONGS.ALBUM_ID
 WHERE RELEASE_YEAR=2020;
 
---18. Create a view called ‘Fav_Songs’ from the songs table having songs with song_id 101-105.
+--18. Create a view called â€˜Fav_Songsâ€™ from the songs table having songs with song_id 101-105.
 CREATE VIEW FAV_SONGS AS 
 SELECT SONG_ID,SONG_TITLE 
 FROM SONGS 
 WHERE SONG_ID BETWEEN 101 AND 105;
 
---19. Update a song name to ‘Jannat’ of song having song_id 101 in Fav_Songs view.
+--19. Update a song name to â€˜Jannatâ€™ of song having song_id 101 in Fav_Songs view.
 UPDATE FAV_SONGS SET SONG_TITLE='JANNAT' WHERE SONG_ID=101;
 
 --20. Find all artists who have released an album in 2020.
@@ -127,3 +127,8 @@ GROUP BY GENRE;
 
 
 --30. Retrieve the total duration of songs by each artist where total duration exceeds 15 minutes.
+SELECT ARTISTS.ARTIST_NAME,SUM(SONGS.DURATION) AS TOTAL_DURATION
+FROM ARTISTS JOIN ALBUMS ON ARTISTS.ARTIST_ID=ALBUMS.ARTIST_ID
+JOIN SONGS ON ALBUMS.ALBUM_ID=SONGS.ALBUM_ID
+GROUP BY ARTIST_NAME
+HAVING SUM(SONGS.DURATION)>15.00;
